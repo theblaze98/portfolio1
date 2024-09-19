0d9805020fde7fe6179d08e83acd21d8298125ec
dionijvargas <dionijvargas@gmail.com>
2024-09-19 15:49:12 +0000

    web analytics vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  })
 });
